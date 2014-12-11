25d6f4e2934e3f676491f6a02500d08eaee407b2
troyready <troy@troyready.com>
2014-12-11 15:00:07 -0800

    add source/issue url to metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,9 @@ maintainer_email 'jim.reeves@biola.edu'
 license          'Apache 2.0'
 description      'Installs/Configures cups'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.0'
+source_url       'https://github.com/biola/chef-cups'
+issues_url       'https://github.com/biola/chef-cups/issues'
+version          '0.4.1'
 
 depends 'git', '~> 4.0'
 
