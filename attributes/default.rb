60acf99393ff2fa40ef2c653fdea9c369e3cba62
Christopher Huhn <C.Huhn@gsi.de>
2015-02-19 17:23:10 +0100

    @LOCAL acl not sufficient for us

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,6 @@ default['cups']['default_printer'] = nil
 default['cups']['loglevel'] = 'info'
 default['cups']['printers'] = []
 default['cups']['systemgroups'] = "sys root"
-default['cups']['share_printers'] = true
+default['cups']['share_printers'] = [ '@LOCAL' ]
 default['cups']['airprint']['airprint_generate']['git_url'] = 'https://github.com/tjfontaine/airprint-generate.git'
 default['cups']['airprint']['airprint_generate']['git_revision'] = 'master'
