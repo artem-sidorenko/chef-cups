0cfdfe2f5803032e6aba6d681d699bd6a44ada26
Christopher Huhn <C.Huhn@gsi.de>
2015-03-09 17:47:30 +0100

    Add option for remote administration

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,9 @@ default['cups']['hostname_lookups'] = false
 default['cups']['loglevel'] = 'info'
 default['cups']['printers'] = []
 default['cups']['systemgroups'] = "sys root"
+# ACLs for printer access:
 default['cups']['share_printers'] = [ '@LOCAL' ]
+# ACLs for remote administration:
+default['cups']['remote_admin']   = [ ]
 default['cups']['airprint']['airprint_generate']['git_url'] = 'https://github.com/tjfontaine/airprint-generate.git'
 default['cups']['airprint']['airprint_generate']['git_revision'] = 'master'
