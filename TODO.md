c7e4c9902e6003504f10051da8008a7414d262fc
Christopher Huhn <C.Huhn@gsi.de>
2015-03-09 18:23:11 +0100

    Add option to block unauthenticated browsing of admin pages

--- TODO.md
+++ TODO.md
@@ -1,4 +1,10 @@
 # Hostname lookups
 
-If we have ACLs like "Allow .example.com", we also need `HostNameLookups On`. 
+If we have ACLs like "Allow .example.com", we also need `HostNameLookups On`.
 This should be automatically detected instead of an explicit attribute.
+
+# cupsctl
+
+cupsctl alters cupsd.conf which will be overwritten by chef.
+Either make cupsctl read-only or preserve its modifications.
+Same is true for the server settings panel in the web interface.
--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,10 @@ default['cups']['printers'] = []
 default['cups']['systemgroups'] = "sys root"
 # ACLs for printer access:
 default['cups']['share_printers'] = [ '@LOCAL' ]
-# ACLs for remote administration:
-default['cups']['remote_admin']   = [ ]
+# ACLs for remote administration -- default is localhost only
+default['cups']['admin']['acl'] = [ ]
+# wether authentication is required for read-only access to the web-interface:
+# Should be enabled as soon as remote admin access is granted
+default['cups']['admin']['auth_read'] = false
 default['cups']['airprint']['airprint_generate']['git_url'] = 'https://github.com/tjfontaine/airprint-generate.git'
 default['cups']['airprint']['airprint_generate']['git_revision'] = 'master'
