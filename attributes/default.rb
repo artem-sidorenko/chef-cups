f35479581433ce047db6829a19cada0884e8de1f
Christopher Huhn <C.Huhn@gsi.de>
2015-04-22 12:35:06 +0200

    Also configure hostname lookups

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,7 @@ default['cups']['default_printer'] = nil
 default['cups']['printers'] = []
 default['cups']['systemgroups'] = "sys root"
 default['cups']['share_printers'] = [ '@LOCAL' ]
+# ACLs like '.example.com' need DNS lookups
+default['cups']['hostname_lookups'] = false
 default['cups']['airprint']['airprint_generate']['git_url'] = 'https://github.com/tjfontaine/airprint-generate.git'
 default['cups']['airprint']['airprint_generate']['git_revision'] = 'master'
