1b9763c011e2cc0d777b8c5e037e8c9dfd657dee
Artem Sidorenko <artem@posteo.de>
2017-05-28 08:44:51 +0200

    Switching to the test cookbook for apt update

---
+++ test/fixtures/cookbooks/test/metadata.rb
@@ -0,0 +1,7 @@
+name             'test'
+maintainer       'Artem Sidorenko'
+maintainer_email 'artem@posteo.de'
+license          'Apache 2.0'
+description      'This cookbook is used for testing purposes'
+long_description 'This cookbook is used for testing purposes'
+version          '0.0.1'
